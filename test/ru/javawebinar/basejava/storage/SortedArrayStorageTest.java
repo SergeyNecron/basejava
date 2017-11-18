39e7a6f3c1ffd49b5be22a516391829b34ef1228
SergeyNecron <SergeyNecron@yandex.ru>
2017-11-18 14:47:52 +0400

    DZ lesson3 Reflection

---
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest {
+    @Test
+    public void saveResume() throws Exception {
+    }
+
+    @Test
+    public void deleteResume() throws Exception {
+    }
+
+    @Test
+    public void getIndex() throws Exception {
+    }
+
+}
