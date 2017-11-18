05dd230b891f56ec15bf1f0be1559fc06418dcf9
SergeyNecron <SergeyNecron@yandex.ru>
2017-11-18 17:10:00 +0400

    DZ lesson3 dz

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -6,14 +6,14 @@ import java.lang.reflect.Field;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
+        r.getClass().getMethod("toString");
         System.out.println(r);
     }
 }
