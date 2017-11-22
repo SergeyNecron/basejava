0f477f1aaecd65d748fb37580c0d08a357bb13c5
SergeyNecron <SergeyNecron@yandex.ru>
2017-11-22 04:49:36 +0400

    DZ lesson4 dz fix

--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -8,17 +8,4 @@ public class SortedArrayStorageTest extends AbstractArrayStorageTest {
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
     }
-
-    @Test
-    public void saveResume() throws Exception {
-    }
-
-    @Test
-    public void deleteResume() throws Exception {
-    }
-
-    @Test
-    public void getIndex() throws Exception {
-    }
-
 }
