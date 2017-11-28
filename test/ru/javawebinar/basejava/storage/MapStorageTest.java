60e87b3350f4461edced9dea5cdfd16c714abe40
SergeyNecron <SergeyNecron@yandex.ru>
2017-11-28 10:29:48 +0400

    Lesson05 test

---
+++ test/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.basejava.storage;
+
+import static org.junit.Assert.*;
+
+public class MapStorageTest extends AbstractStorageTest{
+    public MapStorageTest() {super(new ListStorage());
+    }
+}
