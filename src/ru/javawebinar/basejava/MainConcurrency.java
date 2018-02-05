092e3588a2b6fae7373ea869b5d9803461ce93f1
SergeyNecron <I$d*h1hXVw>
2018-02-05 09:11:58 +0300

    Lesson12 HW11

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -15,7 +15,7 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-//                throw new IllegalStateException();
+                throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -59,8 +59,29 @@ public class MainConcurrency {
         });
         System.out.println(mainConcurrency.counter);
 
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
 
+    }
 
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println("Holding " + lock2);
+                }
+            }
+        }).start();
     }
 
     private synchronized void inc() {
