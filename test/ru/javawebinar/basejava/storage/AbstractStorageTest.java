b18a5db48fe1dcfa3851ff2ac173ede255ec4a7f
SergeyNecron <I$d*h1hXVw>
2018-02-08 17:49:11 +0300

    Lesson13 properties

--- test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
@@ -2,12 +2,13 @@ package ru.javawebinar.basejava.storage;
 
 import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
-import ru.javawebinar.basejava.model.*;
+import ru.javawebinar.basejava.model.ContactType;
+import ru.javawebinar.basejava.model.Resume;
 
 import java.io.File;
-import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,17 +16,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractStorageTest {
-    protected static final File STORAGE_DIR = new File("/home/op/java/basejava/storage");
-
-    private static final Resume R1;
-    private static final Resume R2;
-    private static final Resume R3;
-    private static final Resume R4;
-
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
     private static final String UUID_4 = "uuid4";
+    private static final Resume R1;
+    private static final Resume R2;
+    private static final Resume R3;
+    private static final Resume R4;
 
     static {
         R1 = new Resume(UUID_1, "Name1");
@@ -44,45 +43,45 @@ public abstract class AbstractStorageTest {
         R4.addContact(ContactType.HABRAHABR, "профиль Habrahabr");
         R1.addContact(ContactType.HOME_PHONE, "vasa.ivanov.ru");
 
-        R2.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
-        R3.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
-
-        R1.addSection(SectionType.ACHIEVEMENT, new ListSection(
-                "С 2013 года: разработка проектов \"Практика Java, разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven." +
-                        " Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\" и проведение по ним стажировок" +
-                        " и корпоративных обучений. Более 1000 выпускников.",
-                "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. " +
-                        "Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.",
-                "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. " +
-                        "Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: " +
-                        "Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера."));
-
-        R4.addSection(SectionType.QUALIFICATIONS, new ListSection(
-                "JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2",
-                "Version control: Subversion, Git, Mercury, ClearCase, Perforce",
-                "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB"
-        ));
-
-        R2.addSection(SectionType.EXPERIENCE, new OrganizationSection(
-                new Organization("Java Online Projects", null,
-                        new Organization.Position(13, Month.OCTOBER, "Практика:" +
-                                " Разработка Web приложения \"База данных резюме\"", "Объектная модель, коллекции, система ввода-вывода, работа с файлами," +
-                                " сериализация, работа с XML, JSON, SQL, персистентность в базу данных (PostgreSQL), сервлеты, JSP/JSTL, веб-контейнер Tomcat, HTML, " +
-                                "модульные тесты JUnit, java.util.Logging, система контроля версий Git."),
-                        new Organization.Position(13, Month.OCTOBER, "Стажировка Java Enterprise",
-                                "Разработка полнофункционального Spring/JPA Enterprise приложения c авторизацией и правами доступа на основе ролей с " +
-                                        "использованием наиболее популярных инструментов и технологий Java: Maven, Spring MVC, Security, JPA(Hibernate), REST(Jackson)," +
-                                        " Bootstrap (css,js), datatables, jQuery + plugins, Java 8 Stream and Time API."))));
-
-
-        R3.addSection(SectionType.EDUCATION, new OrganizationSection(
-                new Organization("Alcatel", "http://www.alcatel.ru/",
-                        new Organization.Position(1997, Month.SEPTEMBER, 2005, Month.JANUARY,
-                                "\tИнженер по аппаратному и программному тестированию", "" +
-                                "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM)."),
-                        new Organization.Position(1997, Month.JANUARY, 1998, Month.JANUARY,
-                                "6 месяцев обучения цифровым телефонным сетям (Москва)", ""
-                        ))));
+//        R2.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
+//        R3.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
+//
+//        R1.addSection(SectionType.ACHIEVEMENT, new ListSection(
+//                "С 2013 года: разработка проектов \"Практика Java, разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven." +
+//                        " Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\" и проведение по ним стажировок" +
+//                        " и корпоративных обучений. Более 1000 выпускников.",
+//                "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. " +
+//                        "Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.",
+//                "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. " +
+//                        "Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: " +
+//                        "Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера."));
+//
+//        R4.addSection(SectionType.QUALIFICATIONS, new ListSection(
+//                "JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2",
+//                "Version control: Subversion, Git, Mercury, ClearCase, Perforce",
+//                "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle, MySQL, SQLite, MS SQL, HSQLDB"
+//        ));
+//
+//        R2.addSection(SectionType.EXPERIENCE, new OrganizationSection(
+//                new Organization("Java Online Projects", null,
+//                        new Organization.Position(13, Month.OCTOBER, "Практика:" +
+//                                " Разработка Web приложения \"База данных резюме\"", "Объектная модель, коллекции, система ввода-вывода, работа с файлами," +
+//                                " сериализация, работа с XML, JSON, SQL, персистентность в базу данных (PostgreSQL), сервлеты, JSP/JSTL, веб-контейнер Tomcat, HTML, " +
+//                                "модульные тесты JUnit, java.util.Logging, система контроля версий Git."),
+//                        new Organization.Position(13, Month.OCTOBER, "Стажировка Java Enterprise",
+//                                "Разработка полнофункционального Spring/JPA Enterprise приложения c авторизацией и правами доступа на основе ролей с " +
+//                                        "использованием наиболее популярных инструментов и технологий Java: Maven, Spring MVC, Security, JPA(Hibernate), REST(Jackson)," +
+//                                        " Bootstrap (css,js), datatables, jQuery + plugins, Java 8 Stream and Time API."))));
+//
+//
+//        R3.addSection(SectionType.EDUCATION, new OrganizationSection(
+//                new Organization("Alcatel", "http://www.alcatel.ru/",
+//                        new Organization.Position(1997, Month.SEPTEMBER, 2005, Month.JANUARY,
+//                                "\tИнженер по аппаратному и программному тестированию", "" +
+//                                "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM)."),
+//                        new Organization.Position(1997, Month.JANUARY, 1998, Month.JANUARY,
+//                                "6 месяцев обучения цифровым телефонным сетям (Москва)", ""
+//                        ))));
     }
 
 
